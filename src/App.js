3adf42515659e479cbeaf022f46c53a80f7a50ab
Imad <Imad,salki@hotmail,fr>
2020-04-27 19:45:15 +0200

    INIT

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Counter from './Components/Counter';
+import { Switch, Link, BrowserRouter as Router } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+/* function App */
+const App = () =>  {
+
+  let myObject = {
+    titre: 'Conteur',
+    value: 1
+  };
+
+  return ( 
+    <Router>
+      {/* Navigation Bar */}
+      <nav className="navbar navbar-expand">
+
+        <ul className="navbar-nav">
+
+          <li className="nav-item">
+            <Link to="/home" className="nav-link">Home</Link>
+          </li>
+
+          <li className="nav-item">
+            <Link to="/counter" className="nav-link">Counter</Link>
+          </li>
+
+          <li className="nav-item">
+            <Link to="/about" className="nav-link">About</Link>
+          </li>
+
+        </ul>
+
+      </nav>
+
+      {/* Router Switch */}
+      <div className="container m-3">
+        <Switch>
+
+          {/* Home Path */}
+          <Router path="/home">
+            <h4> Home </h4>
+          </Router>
+
+          {/* Counter Path */}
+          <Router path="/counter">
+            <h4> Counter </h4>
+            <Counter infos={myObject} image="images/bart.png" />
+            <Counter infos={myObject}  image="images/omer.png" />
+          </Router>
+
+          {/* About Path */}
+          <Router path="/about">
+            <h4> About </h4>
+          </Router>
+
+        </Switch>
+
+      </div>
+
+    </Router>
+
   );
 }
 
---
+++ src/Components/Counter.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+
+/* Classe Counter */
+class Counter extends Component {
+
+    // Constructeur de notre component
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: this.props.infos.value,
+            listImages: new Array(this.props.value).fill(0),
+            message: ''
+        };
+    }
+
+    inc = () => {
+        let newValue = this.state.counter + 1;
+        this.setState({
+            counter: newValue,
+            listImages: new Array(newValue >= 0 ? newValue : 0).fill(0)
+        });
+    }
+
+    dec = () => {
+        let newValue = this.state.counter > 0 ? this.state.counter - 1 : 0;
+        this.setState({
+            counter: newValue,
+            listImages: new Array(newValue >= 0 ? newValue : 0 ).fill(0)
+        });
+    }
+
+    // Permet de changer le skill dans l'input
+    changeSkill = (event) => {
+        this.setState({
+            message: event.target.value
+        });
+    }
+    
+    // Le rendu
+    render() {
+        return (
+            <div className="card m-3">
+
+                <div className="card-header"> 
+                    <strong> {this.props.infos.titre} </strong> ===> {this.state.counter}
+                </div>
+
+                <div className="m-2">
+                    <button className="btn btn-primary m-1" onClick={() => this.inc()} > + </button>
+                    <button className="btn btn-primary m-1" onClick={() => this.dec()} > - </button>
+                </div>
+
+                <div className="input-group mb-3">
+                    <input type="text" 
+                        name="skill"
+                        className="form-control" 
+                        placeholder="Ecris ton message ici"     
+                        value={this.state.message} onChange={this.changeSkill}/>
+                    <div className="input-group-append">
+                        <button className="btn btn-primary" onClick={() => alert(this.state.message)} > Show </button>
+                    </div>
+                </div>
+
+                <div className="card-body">
+                    {
+                        this.state.listImages.map( (value) => 
+                            <img width={100} src={this.props.image} alt="this is an Description"></img>
+                        )
+                    }
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default Counter;
