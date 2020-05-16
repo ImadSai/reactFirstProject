ffce7eb001f23b03c274311f84128c927f1c351f
Imad Salki <imad.salki@hotmail.fr>
2020-05-16 17:40:08 +0200

    update Connexion view

--- src/Views/Connexion.js
+++ src/Views/Connexion.js
@@ -5,7 +5,20 @@ class Connexion extends Component {
 
     render() {
         return (
-            <ConnexionComponent />
+            <div className="row">
+
+                <div className="col-lg-8 col-md-8 col-sm-12 p-3">
+                    <h1> Drag and Drop </h1>
+                </div>
+
+                <div className="col-lg-4 col-md-4 col-sm-12 p-3">
+                    <div className="container d-flex justify-content-center mt-2">
+                        <ConnexionComponent />
+                    </div>
+
+                </div>
+
+            </div>
         );
     }
 
